56f5e084d8fa864216d234d05846a36d2785e3c5
Ross Reynolds <rossr@cwgsy.net>
2022-06-15 14:24:16 +0100

    Created SkillData Data Asset

---
+++ Source/RPGProject/Public/DataAssets/SkillData.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Animation/AnimMontage.h"
+#include "SkillData.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class RPGPROJECT_API USkillData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill Data")
+	FName SkillName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill Data")
+	FText SkillDescription;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill Data")
+	UAnimMontage* SkillMontage;
+
+
+
+};
